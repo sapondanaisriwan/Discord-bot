92be5296fd8f2c997149616f9a17fb50fe82fdcd
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2021-10-23 21:45:34 +0700

    EP:2 Explain about await/async

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # 873429697793515551 client id
-# ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.j5Iv8dEWN-QDxn0FnHC4h8JDK60 token
+# ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.aJaBinbcumiUmrsuDWQW7ATIRG4 token
 # 8 permission
 
 import discord
-client = discord.Client()
+client = discord.Client()  
 
 # wrapper / decorator
 @client.event
+# async คือ ทํางานได้หลายฟังชันพร้อมกัน โดยไม่รอ spawn()
 async def on_ready():
     print(f"Logged is as {client.user}")
-client.run("ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.j5Iv8dEWN-QDxn0FnHC4h8JDK60")
+
+# async/await
+# await จะถูกกัาหนดไว้ให้ใช้ตอนไหน ให้ดูใน docuemnt
+@client.event
+async def on_message(message):
+    if message.content == '!send':
+        print(message)
+        await message.channel.send('asd')
+    elif message.content == '!logout':
+        await client.logout()
+
+client.run("ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.aJaBinbcumiUmrsuDWQW7ATIRG4")
