9e785007084ef4c2fdda1b1745440f088f9d65dd
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2021-10-25 17:28:08 +0700

    EP:5 Making play/leave commands

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from sys import prefix
 import discord
+from discord import guild
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
+
 from discord import embeds
 from discord.colour import Color
 from discord.ext import commands
@@ -6,20 +12,23 @@ from datetime import datetime, timedelta
 
 message_lastseen = datetime.now()
 
-bot = commands.Bot(command_prefix='!',help_command=None)
+prefix = '!!'
+bot = commands.Bot(command_prefix=prefix, help_command=None)
 
 # wrapper / decorator
 # #sync คือ ทํางานได้หลายฟังชันพร้อมกัน โดยไม่รอ spawn()
+
+
 @bot.event
 async def on_ready():
     print(f"Logged is as {bot.user}")
 
 @bot.command()
-async def test(ctx, *, agr): # * รับข้อความมาทั้งหมด ถ้าไม่มีก็รับแค่ข้อความหน้าก่อน specbar input: hello world > output: hello
+async def test(ctx, *, agr):  # * รับข้อความมาทั้งหมด ถ้าไม่มีก็รับแค่ข้อความหน้าก่อน specbar input: hello world > output: hello
     await ctx.channel.send(f'You typed {agr}')
 
 @bot.command()
-async def help(ctx): # 0x = base 16
+async def help(ctx):  # 0x = base 16
     emBed = discord.Embed(title='Testing', description='Show all avaiable bot commands', color=0x32a852)
     emBed.add_field(name='help', value='Get help command', inline=False)
     emBed.add_field(name='test', value="Respond message that you've send", inline=False)
@@ -30,6 +39,8 @@ async def help(ctx): # 0x = base 16
 
 # async/await
 # await จะถูกกัาหนดไว้ให้ใช้ตอนไหน ให้ดูใน docuemnt
+
+
 @bot.event
 async def on_message(message):
     global message_lastseen
@@ -41,6 +52,46 @@ async def on_message(message):
         message_lastseen = datetime.now() + timedelta(seconds=1)
     elif message.content == '!logout':
         await bot.logout()
-    await bot.process_commands(message) # ประมวลผลว่าอันไหนควรรันก่อน
+    await bot.process_commands(message)  # ประมวลผลว่าอันไหนควรรันก่อน
 
-bot.run("ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.XEAh6bBJN5QxXNyW3OCXsL0alZk")
+@bot.command(aliases=['play', 'p'])
+async def _play(ctx, url):
+    channel = ctx.author.voice.channel
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client == None:
+        ctx.channel.send('Bot has joined')
+        await channel.connect()
+        voice_client = get(bot.voice_clients, guild=ctx.guild)
+
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': True}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    if not voice_client.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        voice_client.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice_client.is_playing()
+    else:
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        voice_client.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice_client.is_playing()
+        # await ctx.channel.send('Already playing song')
+        # return
+
+@bot.command(aliases=['leave', 'dis', 'disconnect'])
+async def _leave(ctx):
+    if ctx.voice_client:  # If the bot is in a voice channel
+        await ctx.voice_client.disconnect()
+        await ctx.message.add_reaction('👋')
+    else:
+        emBed = discord.Embed(color=0x32a852) 
+        pfp = ctx.message.author.avatar_url # user profile picture
+        emBed.set_author(name=" | There's nothing playing in this server", icon_url=pfp)
+        await ctx.send(embed=emBed)
+
+# :x2: You need to be in the same voice channel as Rythm to use this command
+bot.run("ODczNDI5Njk3NzkzNTE1NTUx.YQ4Syw.RHvCz7ywC1PaW_f1Bkekj3RI1Ac")
